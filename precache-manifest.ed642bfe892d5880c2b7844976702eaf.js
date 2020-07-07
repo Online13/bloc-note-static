2efb20f0053168df9de34644cea910d88681d3a0
Rayane <Rratiarivelo@gmail.com>
2020-07-07 23:15:03 +0300

    Updates

---
+++ precache-manifest.ed642bfe892d5880c2b7844976702eaf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f20fce02ef1d82837672e1ccb908af3",
+    "url": "/bloc-note-static/index.html"
+  },
+  {
+    "revision": "521d92095a32bf911378",
+    "url": "/bloc-note-static/static/css/2.f00e1821.chunk.css"
+  },
+  {
+    "revision": "e480e432c8d6fa7af182",
+    "url": "/bloc-note-static/static/css/main.7578363c.chunk.css"
+  },
+  {
+    "revision": "521d92095a32bf911378",
+    "url": "/bloc-note-static/static/js/2.70e65d52.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/bloc-note-static/static/js/2.70e65d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e480e432c8d6fa7af182",
+    "url": "/bloc-note-static/static/js/main.58bb3a98.chunk.js"
+  },
+  {
+    "revision": "564d87e65813c62adc58",
+    "url": "/bloc-note-static/static/js/runtime-main.c256dbc0.js"
+  },
+  {
+    "revision": "f9a2228d683b138967180ff11697a9db",
+    "url": "/bloc-note-static/static/media/all.f9a2228d.svg"
+  }
+]);
